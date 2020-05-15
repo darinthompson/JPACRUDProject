cb15ef59da017b79bdf9ddae1764cb5016020ba0
darinthompson <darint07@gmail.com>
2020-05-15 17:38:07 -0600

    Added Beer Boot project

---
+++ BeersOfTheWorld/src/main/java/com/skilldistillery/beers_of_the_world/BeersOfTheWorldApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beers_of_the_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersOfTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeersOfTheWorldApplication.class, args);
+	}
+
+}
---
+++ BeersOfTheWorld/src/test/java/com/skilldistillery/beers_of_the_world/BeersOfTheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beers_of_the_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeersOfTheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
